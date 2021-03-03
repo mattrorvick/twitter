0288ad820852292861023823c726027bfafb2cda
mattrorvick <matthewrorvick@gmail.com>
2021-03-03 11:00:54 -0600

    updating user controller

--- src/main/java/com/tts/demoTwitter/controller/UserController.java
+++ src/main/java/com/tts/demoTwitter/controller/UserController.java
@@ -25,19 +25,37 @@ public class UserController {
     @GetMapping(value = "/users/{username}")
     public String getUser(@PathVariable(value="username") String username, Model model) {
         User user = userService.findByUsername(username);
+        User loggedInUser = userService.getLoggedInUser();
         List<Tweet> tweets = tweetService.findAllByUser(user);
+        List<User> following = loggedInUser.getFollowing();
+
+        boolean isFollowing = false;
+        for (User followedUser : following) {
+            if (followedUser.getUsername().equals(username)) {
+                isFollowing = true;
+            }
+        }
+        boolean isSelfPage = loggedInUser.getUsername().equals(username);
+        model.addAttribute("isSelfPage", isSelfPage);
         model.addAttribute("tweetList", tweets);
         model.addAttribute("user", user);
+        model.addAttribute("following", isFollowing);
+
         return "user";
     }
 
 
     @GetMapping(value = "/users")
     public String getUsers(Model model) {
+
+        User loggedInUser = userService.getLoggedInUser();
+        List<User> usersFollowing = loggedInUser.getFollowing();
         List<User> users = userService.findAll();
+        SetFollowingStatus(users, usersFollowing, model);
         model.addAttribute("users", users);
         SetTweetCounts(users, model);
         return "users";
+
     }
 
     private void SetTweetCounts(List<User> users, Model model) {
@@ -48,6 +66,22 @@ public class UserController {
             tweetCounts.put(user.getUsername(), tweets.size());
         }
         model.addAttribute("tweetCounts", tweetCounts);
+    }
+
+    private void SetFollowingStatus(List<User> users, List<User> usersFollowing, Model model) {
+
+        HashMap<String, Boolean> followingStatus = new HashMap<>();
+        String username = userService.getLoggedInUser().getUsername();
+
+        for (User user : users) {
+            if(usersFollowing.contains(user)) {
+                followingStatus.put(user.getUsername(), true);
+            }else if (!user.getUsername().equals(username)) {
+                followingStatus.put(user.getUsername(), false);
+            }
+        }
+        model.addAttribute("followingStatus", followingStatus);
+
     }
 
 }
